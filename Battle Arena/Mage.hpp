9733939e41f4b25a9810007a1c95733833e7991f
Shoaib Hayat <shoaib.2201190@gmail.com>
2025-04-23 18:08:44 +0500

    Update Mage.hpp

--- Battle Arena/Mage.hpp
+++ Battle Arena/Mage.hpp
@@ -1,17 +1,20 @@
 #pragma once
 
 #include "Player_Character.hpp"
+#include "FireStaff.hpp"
+#include "IceStaff.hpp"
+#include "Weapon.hpp"
 
 class Mage : public Character {
 	protected:
 
 	public:
-		Mage(std::string name, float health = 0.f, float damage = 0.f) : Character(name, damage, health) {}
+		Mage(std::string name, float health = 0.f, float damage = 0.f, std::string type) : Character(name, damage, health, type, new FireStaff()) {}
 		
 		void attack(Character& opponent);
 		void healing();
 		void movecharacter();
 
-		~Mage() {}
+		~Mage() override = default;
 };
 
