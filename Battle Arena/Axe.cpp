9a67428ef435e6f2465e0935e1e75fde873fecae
Shoaib Hayat <shoaib.2201190@gmail.com>
2025-04-23 18:04:10 +0500

    Update Axe.cpp

--- Battle Arena/Axe.cpp
+++ Battle Arena/Axe.cpp
@@ -1,1 +1,15 @@
 #include "Axe.hpp"
+
+
+void Axe::displayStats() const {
+    Weapon::displayStats();
+    // cout << "Special Move: Whirlwind Attack" << endl;
+    // cout << "Description: A powerful spinning attack that deals damage to all enemies in range." << endl;
+}
+
+void Axe::useSpecialMove(Character* target) {
+    // cout << "Using Whirlwind Attack on " << target->getname() << "!" << endl;
+    // Implement the logic for the special move here
+    // For example, you could deal damage to the target
+    // target->takeDamage(damage * 2); // Example: double damage for special move
+}
