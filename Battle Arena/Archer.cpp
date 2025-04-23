6bfb0c7277b1f3ce35d0b3aad824d9158723ee86
Shoaib Hayat <shoaib.2201190@gmail.com>
2025-04-23 18:02:58 +0500

    Update Archer.cpp

--- Battle Arena/Archer.cpp
+++ Battle Arena/Archer.cpp
@@ -1,13 +1,20 @@
 #include "Archer.hpp"
 
 void Archer::attack(Character& opponent) {
-
+    attackWithCurrentWeapon(&opponent);
 }
 
 void Archer::healing() {
-
+    if (Character::gethealth() < 100.f) {
+        Character::sethealth(Character::gethealth() + 10.f);
+    }
+    else {
+        Character::sethealth(100.f);
+    }
+    std::cout << Character::getname() << " healed to " << Character::gethealth() << " health." << std::endl;
+    std::cout << std::endl;
 }
 
 void Archer::movecharacter() {
-
+    
 }
