573631986e0fb70112ad791d5c3919df40931dc6
Shoaib Hayat <shoaib.2201190@gmail.com>
2025-04-23 18:03:40 +0500

    Update Archer.hpp

--- Battle Arena/Archer.hpp
+++ Battle Arena/Archer.hpp
@@ -1,17 +1,22 @@
 #pragma once
 
 #include "Player_Character.hpp"
+#include "LongBow.hpp"
+#include "ShortBow.hpp"
+#include "Weapon.hpp"
 
 class Archer : public Character {
 	protected:
 
 	public:
-		Archer(string name, float health = 0.f, float damage = 0.f) : Character(name, damage, health) {}
+		Archer(string name, float health = 0.f, float damage = 0.f, string Type, Weapon* weapon2 = nullptr): Character(name, health, damage, Type, new LongBow()) {
+			this->weapon2 = weapon2;
+		}
 		
 		void attack(Character& opponent);
 		void healing();
 		void movecharacter();
 
-		~Archer() {}
+		~Archer() override = default;
 };
 
