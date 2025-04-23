e5dc7778b0de070115f365006f775dbb95908caa
huzai <huzai@DESKTOP-4H0ST29>
2025-04-23 12:48:27 +0500

    Updated Weapon Classes

--- Battle Arena/Player_Character.cpp
+++ Battle Arena/Player_Character.cpp
@@ -1,5 +1,14 @@
 #include "Player_Character.hpp"
 
+bool Character::isAlive() const {
+	return health > 0;
+}
+void Character::displayStats() const {
+	cout << "Name: " << name << endl;
+	cout << "Health: " << health << endl;
+	cout << "Damage: " << damage << endl;
+}
+
 string Character::getname() const {
 	return name;
 }
--- Battle Arena/Player_Character.hpp
+++ Battle Arena/Player_Character.hpp
@@ -14,8 +14,12 @@ class Character {
 		Character(string n, float health = 0.f, float damage = 0.f) : name(name), health(health), damage(damage) {}
 		
 		virtual void attack(Character& opponent) = 0;
+		virtual void takeDamage(float damage) = 0;
+		virtual void useSpecialAbility() = 0;
 		virtual void movecharacter() = 0;
 		virtual void healing() = 0;
+		void displayStats() const;
+		bool isAlive() const;
 		
 		string getname() const;
 		float gethealth() const;
