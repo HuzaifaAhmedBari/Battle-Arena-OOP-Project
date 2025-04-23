b9b98ef2c70cc338f7834ee8d30c14568720021c
Shoaib Hayat <shoaib.2201190@gmail.com>
2025-04-23 18:06:12 +0500

    Update IceStaff.cpp

--- Battle Arena/IceStaff.cpp
+++ Battle Arena/IceStaff.cpp
@@ -1,1 +1,15 @@
 #include "IceStaff.hpp"
+
+
+
+void IceStaff::displayStats() const {
+    Weapon::displayStats();
+    // cout << "Special Move: Frostbite" << endl;
+    // cout << "Description: A chilling attack that slows down the target." << endl;
+}
+void IceStaff::useSpecialMove(Character* target) {
+    // cout << "Using Frostbite on " << target->getname() << "!" << endl;
+    // Implement the logic for the special move here
+    // For example, you could deal damage to the target
+    // target->takeDamage(damage * 2); // Example: double damage for special move
+}
