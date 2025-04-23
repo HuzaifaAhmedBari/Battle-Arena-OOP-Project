8e476dcefdd6f36cab684e1282fc1b511dcb5a68
Shoaib Hayat <shoaib.2201190@gmail.com>
2025-04-23 18:18:08 +0500

    Update Warrior.cpp

--- Battle Arena/Warrior.cpp
+++ Battle Arena/Warrior.cpp
@@ -1,13 +1,22 @@
 #include "Warrior.hpp"
+#include "Player_Character.hpp"
 
 void Warrior::attack(Character& opponent) {
-	
+	attackWithCurrentWeapon(&opponent);
 }
 
 void Warrior::healing() {
-
+    if (Character::gethealth() < 100.f) {
+        Character::sethealth(Character::gethealth() + 10.f);
+    }
+    else {
+        Character::sethealth(100.f);
+    }
+    std::cout << Character::getname() << " healed to " << Character::gethealth() << " health." << std::endl;
+    std::cout << std::endl;
 }
 
-void Warrior::movecharacter() {
+
+void Warrior::moveCharacter() {
 
 }
