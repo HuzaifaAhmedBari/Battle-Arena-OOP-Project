0e72c6f899740df8b21af9b231020fb95e4faddc
Shoaib Hayat <shoaib.2201190@gmail.com>
2025-04-23 18:07:22 +0500

    Update LongBow.cpp

--- Battle Arena/LongBow.cpp
+++ Battle Arena/LongBow.cpp
@@ -1,3 +1,15 @@
 #include "LongBow.hpp"
 
 
+
+void LongBow::displayStats() const {
+    Weapon::displayStats();
+    // cout << "Special Move: Piercing Arrow" << endl;
+    // cout << "Description: A powerful shot that pierces through armor." << endl;
+}
+void LongBow::useSpecialMove(Character* target) {
+    // cout << "Using Piercing Arrow on " << target->getname() << "!" << endl;
+    // Implement the logic for the special move here
+    // For example, you could deal damage to the target
+    // target->takeDamage(damage * 2); // Example: double damage for special move
+}
