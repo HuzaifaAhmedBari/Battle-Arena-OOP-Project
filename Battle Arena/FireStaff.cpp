67b3fcbaeb92cd2be9415f2102e4938b2e47accb
Shoaib Hayat <shoaib.2201190@gmail.com>
2025-04-23 18:05:03 +0500

    Update FireStaff.cpp

--- Battle Arena/FireStaff.cpp
+++ Battle Arena/FireStaff.cpp
@@ -1,1 +1,14 @@
 #include "FireStaff.hpp"
+
+
+void FireStaff::displayStats() const {
+    Weapon::displayStats();
+    // cout << "Special Move: Fireball" << endl;
+    // cout << "Description: A powerful fire attack that deals damage to a single target." << endl;
+}
+void FireStaff::useSpecialMove(Character* target) {
+    //cout << "Using Fireball on " << target->getname() << "!" << endl;
+    // Implement the logic for the special move here
+    // For example, you could deal damage to the target
+    // target->takeDamage(damage * 2); // Example: double damage for special move
+}
