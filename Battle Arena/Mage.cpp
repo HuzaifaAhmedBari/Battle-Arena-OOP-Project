3f9e81a217d7e5a15f6309a79e6e8125c4da2fd6
Shoaib Hayat <shoaib.2201190@gmail.com>
2025-04-23 18:08:15 +0500

    Update Mage.cpp

--- Battle Arena/Mage.cpp
+++ Battle Arena/Mage.cpp
@@ -1,13 +1,21 @@
 #include "Mage.hpp"
 
 void Mage::attack(Character& opponent) {
-
+    attackWithCurrentWeapon(&opponent);
 }
 
 void Mage::healing() {
-
+    if (Character::gethealth() < 100.f) {
+        Character::sethealth(Character::gethealth() + 10.f);
+    }
+    else {
+        Character::sethealth(100.f);
+    }
+    std::cout << Character::getname() << " healed to " << Character::gethealth() << " health." << std::endl;
+    std::cout << std::endl;
 }
 
 void Mage::movecharacter() {
+    
 
 }
