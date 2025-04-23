b82f9f253b79d074497a5d0072218075ba8cd190
Shoaib Hayat <shoaib.2201190@gmail.com>
2025-04-23 18:19:52 +0500

    Update Weapon.hpp

--- Battle Arena/Weapon.hpp
+++ Battle Arena/Weapon.hpp
@@ -6,15 +6,25 @@
 #include <iostream>
 using namespace std;
 
+class Character;
+
 class Weapon
 {
 	protected:
 		string name;
 		float range, damage;
+		float damageIncrement = 5.0f;
+		int unlockLevel = 1, specialMoveUnlockLevel = 3;
+		bool specialUnlocked = false;
 	public:
 		Weapon() {}
 		Weapon(string name, float range, float damage) : name(name), range(range), damage(damage) {}
 
+		virtual void displayStats() const;
+		virtual void useSpecialMove(Character* target) = 0;
+		virtual void levelUpEffect(int playerLevel);
+    	bool isSpecialUnlocked() const;
+    	// virtual void useSpecialMove(Character* target) override;
 		string getname() const;
 		float getrange() const;
 		float getdamage() const;
