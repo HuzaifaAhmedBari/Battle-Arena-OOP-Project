fc6dabb165667b3df77b58581746f3bef5548f59
Shoaib Hayat <shoaib.2201190@gmail.com>
2025-04-23 18:15:35 +0500

    Update Sword.cpp

--- Battle Arena/Sword.cpp
+++ Battle Arena/Sword.cpp
@@ -1,3 +1,15 @@
 #include "Sword.hpp"
 
 
+
+void Sword::displayStats() const {
+    Weapon::displayStats();
+    // cout << "Special Move: Sword Slash" << endl;
+    // cout << "Description: A powerful slash that deals extra damage." << endl;
+}
+void Sword::useSpecialMove(Character* target) {
+    // cout << "Using Sword Slash on " << target->getname() << "!" << endl;
+    // Implement the logic for the special move here
+    // For example, you could deal damage to the target
+    // target->takeDamage(damage * 2); // Example: double damage for special move
+}
