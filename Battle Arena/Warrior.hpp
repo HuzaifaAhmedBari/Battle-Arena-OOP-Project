e733b8bf719524a1090841222fc34aaa9116f36c
Shoaib Hayat <shoaib.2201190@gmail.com>
2025-04-23 18:18:33 +0500

    Update Warrior.hpp

--- Battle Arena/Warrior.hpp
+++ Battle Arena/Warrior.hpp
@@ -1,17 +1,19 @@
 #pragma once
 
 #include "Player_Character.hpp"
-
+#include "Sword.hpp"
+#include "Axe.hpp"
+#include "Weapon.hpp"
 class Warrior : public Character {
 	protected:
 
 	public:
-		Warrior(std::string name, float health = 0.f, float damage = 0.f) : Character(name,damage,health) {}
+		Warrior(std::string name, float health = 0.f, float damage = 0.f) : Character(name, health, damage, "Warrior", new Sword()) {}
 		
 		void attack(Character &opponent);
 		void healing();
-		void movecharacter();
+		void moveCharacter();
 
-		~Warrior() {}
+		~Warrior() override = default;
 };
 
