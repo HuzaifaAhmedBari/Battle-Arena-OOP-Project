626d70c9706b2b60f19b860f974b2fcda953adf6
Shoaib Hayat <shoaib.2201190@gmail.com>
2025-04-23 18:11:28 +0500

    Update ShortBow.cpp

--- Battle Arena/ShortBow.cpp
+++ Battle Arena/ShortBow.cpp
@@ -1,1 +1,15 @@
 #include "ShortBow.hpp"
+
+
+
+void ShortBow::displayStats() const {
+    Weapon::displayStats();
+    // cout << "Special Move: Rapid Fire" << endl;
+    // cout << "Description: A quick succession of arrows that deal damage to multiple targets." << endl;
+}
+void ShortBow::useSpecialMove(Character* target) {
+    // cout << "Using Rapid Fire on " << target->getname() << "!" << endl;
+    // Implement the logic for the special move here
+    // For example, you could deal damage to the target
+    // target->takeDamage(damage * 2); // Example: double damage for special move
+}
