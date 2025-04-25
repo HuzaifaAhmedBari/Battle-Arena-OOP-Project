a4e8716b6d8b9bf3b9f8f6fd90193bf23266fd8b
Shoaib Hayat <shoaib.2201190@gmail.com>
2025-04-25 15:34:10 +0500

    Update Sword.cpp

--- Battle Arena/Sword.cpp
+++ Battle Arena/Sword.cpp
@@ -8,6 +8,14 @@ void Sword::displayStats() const {
     // cout << "Description: A powerful slash that deals extra damage." << endl;
 }
 void Sword::useSpecialMove(Character* target) {
+    if (!canUseSpecial()) {
+        std::cout << "Special move unavailable or no uses left.\n";
+        return;
+    }
+    
+    std::cout << name << " uses its special power!\n";
+    // ... apply effect ...
+    reduceSpecialUse();
     // cout << "Using Sword Slash on " << target->getname() << "!" << endl;
     // Implement the logic for the special move here
     // For example, you could deal damage to the target
