19358426de592e28859fc5412d0a6c592155d209
Shoaib Hayat <shoaib.2201190@gmail.com>
2025-04-25 15:31:29 +0500

    Update IceStaff.cpp

--- Battle Arena/IceStaff.cpp
+++ Battle Arena/IceStaff.cpp
@@ -8,6 +8,14 @@ void IceStaff::displayStats() const {
     // cout << "Description: A chilling attack that slows down the target." << endl;
 }
 void IceStaff::useSpecialMove(Character* target) {
+    if (!canUseSpecial()) {
+        std::cout << "Special move unavailable or no uses left.\n";
+        return;
+    }
+    
+    std::cout << name << " uses its special power!\n";
+    // ... apply effect ...
+    reduceSpecialUse();
     // cout << "Using Frostbite on " << target->getname() << "!" << endl;
     // Implement the logic for the special move here
     // For example, you could deal damage to the target
