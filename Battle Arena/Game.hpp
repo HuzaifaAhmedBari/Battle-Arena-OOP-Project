fefc0b287f0bed7025457fbb803332d932feaf5b
Huzaifa Ahmed Bari <huzaifa.ahmed.bari10@gmail.com>
2025-04-25 23:00:03 +0500

    Update Game.hpp

--- Battle Arena/Game.hpp
+++ Battle Arena/Game.hpp
@@ -19,13 +19,26 @@ enum GameState {
     SelectCharacter,
     SelectWeapon
 };
+enum CharacterState {
+	Warrior,
+	Mage,
+	Archer
+};
+enum WeaponState {
+	Sword,
+    Axe,
+	FireStaff,
+    IceStaff,
+	ShortBow,
+    LongBow
+};
 
 class Game
 {
     public:
         void run();
         void start_screen(RenderWindow &window,GameState &currentstate, Sprite &Start_BackGround_sprite);
-        void select_character_screen(RenderWindow &window, GameState &currentstate, Sprite &Select_Character_sprite);
+        void select_character_screen(RenderWindow &window, GameState &currentstate, CharacterState &currrentcharacter, Sprite &Select_Character_sprite);
         void select_weapon_screen();
 };
 
